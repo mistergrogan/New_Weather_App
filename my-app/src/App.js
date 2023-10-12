07a90d07bb8e8eae796f852606523f32e02c3069
mistergrogan <thomasmgrogan1@gmail.com>
2023-10-12 17:38:03 +0100

    images uploaded for weather type

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+const weatherAPI = {
+  key: `${process.env.WEATHER_APP_API_KEY}`,
+  base: `${process.env.WEATHER_APP_AUTH_DOMAIN}`
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box"/>
+          <input
+            type="text"
+            className="search-bar"
+            placeholder='Search...'/>
+      </main>
     </div>
   );
 }
